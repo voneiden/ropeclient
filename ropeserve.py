878e3547615c321527fa46e0257b44a218aae204
Matti Eiden <snaipperi@gmail.com>
2010-11-22 22:01:15 +0200

    ropeclient git started

---
+++ ropeserve.py
@@ -0,0 +1,294 @@
+from twisted.internet.protocol import Factory, Protocol
+from twisted.internet import reactor
+from twisted.protocols.basic import LineReceiver
+import hashlib, random
+
+accounts = {
+    'patseli':'774a3cdfc9d251d95a823f1a5178d4703fdd892c657bd46fa3f5fde6a77ccfab',
+    'pietu':'774a3cdfc9d251d95a823f1a5178d4703fdd892c657bd46fa3f5fde6a77ccfab'}
+
+players = []
+
+NOECHO = '\xff\xfb\x01'
+ECHO   = '\xff\xfc\x01'
+
+CSI = '\033['
+
+COLOR = {
+    'black':'001',
+    'red':'002',
+    'green':'003',
+    'yellow':'004',
+    'blue':'005',
+    'magneta':'006',
+    'cyan':'007',
+    'white':'008',
+    'gray':'009',
+    'dim gray':'010'}
+
+
+
+
+def ansi(code):
+    return CSI + code 
+
+
+
+class ServeGame(LineReceiver):
+    def connectionMade(self):
+        self.handle = self.login
+        self.state = 0
+
+        #self.write(self.factory.text)
+        #self.write("Login: ")
+        #self.transport.loseConnection()
+        self.typing = False
+        
+    def lineReceived(self, data):
+        print("Line received!")
+        self.handle(data)
+    
+    def connectionLost(self,reason):
+        if self in players:
+            players.remove(self)
+            print (self.nick,"disconnected")
+            self.announce("(%s has disconnected!)"%self.nick)
+            self.announce_players()
+    
+    def write(self,data,newline=True):
+        data = data.encode('utf-8')
+        if newline: self.transport.write(data+'\r\n')
+        else: self.transport.write(data)
+
+
+    def login(self,data):
+        tok = data.split(' ')
+        self.typing = False
+        if tok[0] == 'TYPING': self.typing = True
+        elif tok[0] == 'NOT_TYPING': self.typing = False
+
+        if self.state == 0:
+            print ("Got data",data)
+            if data != "SUPERHANDSHAKE": self.transport.loseConnection()
+            self.state = 1
+        else:
+            tok = data.split(' ')
+            if tok[0] == 'SETNICK':
+                self.nick = tok[1]
+                self.handle = self.game
+                players.append(self)
+                self.announce("(%s has joined the game!)"%self.nick)
+                pl = []
+                for player in players: pl.append(player.nick)
+                self.announce("D_PLAYERS %s"%(" ".join(pl)))
+            elif tok[0] == 'SETNAME':
+                self.name = " ".join(tok[1:])   
+            
+    def announce(self,data):
+        text = self.wrap(data)
+        for player in players: player.write(text)
+    def wrap(self,data):
+        ''' Miten ois v2 mika lukee koko paskan kirjain kirjaimelta -> varien sailytys onnistuis '''
+        # First talking
+        quote = False
+        offtopic = False
+        dice  = False
+        output = ''
+        dbuf = ''
+        color = [ansi(COLOR['white'])]
+        print data
+        '''
+        for tok in data.split(' '):
+            if len(tok) == 0: print "funny",data.split(' ');continue
+            if tok[0] == '!':
+                if len(tok) > 2:
+                    # Probably a dice, hit it, machine!
+                    roll = self.dice(tok)
+                    if roll:
+                        color.append(ansi(COLOR['red']))
+                        output += color[-1] + roll
+                        color.pop()
+                        output += color[-1] + ' '
+                        continue
+            '''     
+        for char in data:
+
+            if char == '!' and not dice:
+                dbuf = '!'
+                dice = 1
+                continue
+
+            elif dice == 1:
+                if char.isdigit():
+                    dbuf += char
+                    dice = 2
+                    continue
+                elif char == 'd':
+                    dbuf += char
+                    dice = 3
+                    continue
+                else:
+                    output += dbuf
+                    dbuf = ''
+                    dice = False
+                    
+                
+            elif dice == 2:
+                if char == 'd':
+                    dbuf += char
+                    dice = 3
+                    continue
+                else:
+                    output += dbuf
+                    dbuf = ''
+                    dice = False
+
+            elif dice == 3:
+                if char.isdigit():
+                    dbuf += char
+                    continue
+                else:
+                    dice = False
+                    roll = self.dice(dbuf)
+                    if roll:
+                        color.append(ansi(COLOR['red']))
+                        output += color[-1] + roll
+                        color.pop()
+                        output += color[-1]
+                    else:
+                        output += dbuf
+
+                    dbuf = ''
+
+
+            
+            if char == '"' and not quote:
+                quote = True
+                color.append(ansi(COLOR['cyan']))
+                output +=  color[-1]+ '"'
+                continue
+            elif char == '"' and quote:
+                quote = False
+                color.pop()
+                output += '"' + color[-1]
+                continue
+
+            elif char == '(' and not offtopic:
+                offtopic = True
+                color.append(ansi(COLOR['gray']))
+                output += color[-1] + "("
+                continue
+            elif char == ')' and offtopic:
+                offtopic = False
+                color.pop()
+                output += ")" + color[-1]
+                continue
+            
+                
+            else: output += char
+            #output += ' '
+        #output += dbuf
+        if len(dbuf) > 0: output += self.dice(dbuf)
+        print output
+        return output
+                
+        '''
+        print(data)
+        buffer = []
+        data = data.split('"')
+        for i,tok in enumerate(data):
+            if   i == 0: buffer.append(tok)
+            elif i % 2 : buffer.append(ansi(COLOR['cyan']) + '"' + tok + '"' + ansi(COLOR['white']))
+            else:        buffer.append(tok)
+        data = "".join(buffer)
+        print(data)
+
+        buffer = []
+        data = data.split('(')
+        print( "split",data)
+        for i,tok in enumerate(data):
+            if i == 0: buffer.append(tok)
+            else:
+                data2 = tok.split(')')
+                print ("split2",data2)
+                for j,tok2 in enumerate(data2):
+                    if j == 0: buffer.append(ansi(COLOR['gray']) + '(' + tok2)
+                    else: buffer.append(')' + ansi(COLOR['white']) + tok2)
+
+        data = "".join(buffer)
+        return data
+        '''
+    def game(self,data):
+        if len(data) == 0: return
+        data = data.decode('utf-8')
+        tok = data.split(' ')
+        if tok[0] == 'TYPING': self.typing = True;self.announce_players()
+        elif tok[0] == 'NOT_TYPING': self.typing = False;self.announce_players()
+        elif tok[0] == '/name': self.name = " ".join(tok[1:])
+        else: 
+            if data[0] == '*': self.announce('''%s %s'''%(self.name,data[1:]))
+            elif data[0] == '(': self.announce('''(%s: %s'''%(self.name,data[1:]))
+            else: self.announce('''%s says, "%s"'''%(self.name,data))
+            self.typing = False
+            self.announce_players()
+    def announce_players(self):
+        pl = []
+        for player in players: 
+            if player.typing: pl.append("*" + player.nick)
+            else: pl.append(player.nick)
+        self.announce("D_PLAYERS %s"%(" ".join(pl)))
+    def dice(self,x):
+        roll = [] # Dices we still need to roll [add/subtract, dice string]
+        rolled = [] # Dices we have rolled: [add/subtract, dice string, result]
+
+        y = x[1:].split('+') # Ignore the ! in the beginning, split the shit from + signs
+        for tok in y:        # for every PLUS..
+            z=tok.split('-') # Split the splitted shit from - signs
+            for i,obj in enumerate(z):          # For every MINUS after a PLUS
+                if len(obj) == 0: continue      # skip empty shit (for ex: !-d6 looks like [EMPTY,d6]
+                if i == 0: roll.append([1,obj]) # if it's the first it's a PLUS
+                else: roll.append([-1,obj])     # all the rest is MINUS
+        while len(roll) > 0:       # While we have something to roll, do it.
+            a,d = roll.pop(0)      # Pop the first out from the queue list
+            if 'd' in d:           # If there is a "d", it's a dice (ex. 2d6)
+                tok = d.split('d') # So lets split the shit (xdy)
+                try: x=int(tok[0]) # Just in case it's soemthing like "d6", we assume x to be one (1d6)
+                except: x = 1      # Yep.
+                if 1 > x or x > 10: rolled.append([a,">%s<"%d,0]); continue # If the number of dices to roll is too big
+                try: y = int(tok[1])                                        # Check that the y is a number
+                except: rolled.append([a,"?%s?"%d,0]); continue             # if it's fucked up, screw that.
+                if 2 > y or  y > 200: rolled.append([a,">%s<"%d,0]); continue # check that it's not a crazy value
+                for i in xrange(x):                     # Do the dice rolling loop
+                    r=random.randint(1,y)               # random!
+                    if r == y: roll.append((a,'d%i'%y)) # it's exploding!
+                    rolled.append([a,'d%i'%y,r])        # done rolling 
+            else:                                                           # Guess it's not a dice (no "d")
+                try:                                                        # Lets try
+                    i = int(d)                                              # Maybe it's a number
+                    rolled.append([a,d,i])                                  # No errors, cool!
+                except: rolled.append([a,"error!%s!error"%d,0]); continue   # Nope, it's something weird.
+        
+        tot = 0                                             # Total result
+        output = []                                         # Info string
+        for obj in rolled:                                  # for everything in the rolled list
+            if obj[0] == 1:                                 # if it's 1, it means it's ADD. else SUBTRACT
+                tot += obj[2]                               # So add the result to total
+                output.append("+[%s:%i]"%(obj[1],obj[2]))   # and write info to 
+            else:                                           # its SUBTRAACT
+                tot -= obj[2]                               # minus it
+                output.append("-[%s:-%i]"%(obj[1],obj[2]))  # write it
+        if tot == 0: return                                 # dont bother to send if there are no results   
+        #msg.Chat.SendMessage("%s rolls: %i (%s)"%(msg.FromDisplayName,tot,"".join(output))) #combine and send it
+        return "%s (%s)"%(str(tot), "".join(output))
+
+
+class ServeGameFactory(Factory):
+    protocol = ServeGame
+    def __init__(self, text=None):
+        if text is None:
+            text = """Sup bro. Please use a %stelnet/mud%s client that has back background. Using a command line is a good idea too."""%(ansi(COLOR['red']),ansi(COLOR['white']))
+        self.text = text
+
+if __name__ == '__main__':
+    reactor.listenTCP(49500, ServeGameFactory())
+    reactor.run()
