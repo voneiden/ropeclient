ddd5be908c41c60ef7500f2a00504aad74151a00
Matti Eiden <snaipperi@gmail.com>
2010-11-27 17:12:16 +0200

    tell typing bug

--- ropeserve.py
+++ ropeserve.py
@@ -215,7 +215,7 @@ class ServeGame(LineReceiver):
         elif tok[0] == 'NOT_TYPING': self.typing = False;self.announce_players()
         elif tok[0] == '/name': self.setname(" ".join(tok[1:]))#self.name = ;self.regex = re.compile(self.name,re.IGNORECASE)
         elif tok[0] == '/gm': self.gm = (self.gm+1)%2;self.typing = False;self.announce_players()
-        elif tok[0] == '/tell': self.tell(tok[1:])
+        elif tok[0] == '/tell': self.tell(tok[1:]);self.typing = False;self.announce_players()
         else: 
             if data[0] == '*': self.announce('''%s %s'''%(self.name,data[1:]))
             elif data[0] == '!': 
