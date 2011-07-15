0ac15d739c8b52a1e57cea809a4ae34d535ebebe
Matti Eiden <snaipperi@gmail.com>
2011-07-15 21:46:20 +0300

    Added custom typing styles

--- server/db.py
+++ server/db.py
@@ -111,3 +111,5 @@ class Account:
     def __init__(self,name,password):
         self.name = name
         self.password = password
+        self.style = 0
+        
--- server/player.py
+++ server/player.py
@@ -34,7 +34,7 @@ class Player(object):
         self.core = core
         self.world = self.core.world
         self.db = self.core.db
-        self.name = 'Unknown'
+        self.name = "Undefined!"
         self.password = None
         self.account = None
         self.typing = False
@@ -43,6 +43,17 @@ class Player(object):
         self.gamemaster = False
         self.handlerstate = 1
         self.temp = {}
+        
+        self.commands = {
+            'chars':self.handleCharlist,
+            'intro':self.handleIntroduce,
+            'memorize':self.handleMemorize,
+            'gm':self.handleGM,
+            'spawn':self.handleSpawn,
+            'look':self.handleLook,
+            'style':self.handleStyle
+            }
+        
     def __getstate__(self): 
         """ Players will never be pickled as they contain references to networking """
         return None
@@ -140,6 +151,7 @@ class Player(object):
             
         elif self.handlerstate == 12:
             if self.temp['password'] == message[0]:
+                print "New accont with",self.temp['name'],self.temp['password']
                 self.account = db.Account(self.temp['name'],self.temp['password'])
                 self.db.accounts.append(self.account)
                 self.db.save()
@@ -158,13 +170,15 @@ class Player(object):
         
     def login(self):
         # We need to check for old player connections and disconnect them.
-        old = self.world.find(self.name,self.world.players)
+        old = self.world.find(self.account.name,self.world.players)
         print "Looking for old",old
         if isinstance(old,Player):
             old.disconnect()
-        character = self.db.findOwner(self.name,self.core.world.characters)
+        
+        self.name = self.account.name 
+        character = self.db.findOwner(self.account.name,self.core.world.characters)
         if character == None:
-            newcharacter = world.Character(self.world,"Soul of %s"%(self.name),self.account.name)
+            newcharacter = world.Character(self.world,self.account.name,"Soul of %s"%(self.account.name))
             newcharacter.mute = True
             newcharacter.attach(self)
         elif isinstance(character,world.Character):
@@ -175,69 +189,27 @@ class Player(object):
         print "Sending clk"
         self.connection.write("clk test;yellow;/testing;Click here to test a command!")
         
-    def gameHandler(self, message):
+    def gameHandler(self, tok):
         if self.character:
-            if message[0][0] == '(':
-                if message[-1][-1] == ')': 
-                    message[-1] = message[-1][:-1]
-                message = "%s: %s"%(self.name," ".join(message)[1:])
-                self.world.offtopic(message)
-            
-            elif message[0] == '/chars':
-                print "Listing chars"
-                chars = self.world.findOwner(self.name,self.world.characters)
-                if chars == None: 
-                    self.offtopic("You possess no characters..")
-                    return
-                buffer = ["You can possess the following characters"]
-                if not isinstance(chars,list): 
-                    chars = [chars]
-                for char in chars:
-                    if char == self.character:
-                        buffer.append("** %s - %s **"%(char.name,char.info))
-                    else:
-                        buffer.append("%s - %s"%(char.name,char.info))
-                self.offtopic('\n'.join(buffer))
-                
-            elif message[0] == '/introduce':
-                name = " ".join(message[1:])
-                if len(name) < 2: return "Introduce as who?"
-                pass
-                
-            elif message[0] == '/memorize':
-                pass
-                
-            elif message[0] == '/gm':
-                self.gamemaster = True
-                
-            elif message[0] == '/spawn':
-                self.handler = self.gmSpawner
-                self.handlerstate = 0
-                self.temp = {}
-                return self.handler(None)
-            elif message[0] == '/look':
-                loc = self.character.location
-                buffer = [loc.name]
-                buffer.append(loc.description)
-                if len(loc.characters) == 1:
-                    buffer.append("You're alone")
-                elif len(loc.characters) == 2:
-                    chars = loc.characters[:]
-                    chars.remove(self.character)
-                    buffer.append("%s is here."%chars[0].name)
-                else:
-                    chars = loc.characters[:]
-                    chars.remove(self.character)
-                    buffer.append("%s are here."%(", ".join(chars)))
-                self.send("\n".join(buffer))
+            for command in self.commands.keys():
+                if self.account.style == 0:
+                    if re.search("^/%s"%command,tok[0]):
+                        return self.commands[command](tok)
+                elif self.account.style == 1:
+                    if re.search("^%s"%command,tok[0]):
+                        return self.commands[command](tok)
+                    
+            if tok[0][0] == '(':
+                return self.handleOfftopic(tok)
                 
             elif not self.character.mute:        
                 self.character.location.announce('''%s says, "%s"'''%(self.character.name, " ".join(message)))
             else:
-                message = "%s: %s"%(self.name," ".join(message))
+                message = "%s: %s"%(self.account.name," ".join(tok))
                 self.world.offtopic(message)
                 
     def gmSpawner(self,message):
+        ''' This is a handler for character generation '''
         self.handlerstate += 1
         if isinstance(message,list):
             msg = " ".join(message)
@@ -264,3 +236,75 @@ class Player(object):
             newchar.move(self.character.location)
             self.handler = self.gameHandler
             return "Done"
+            
+            
+    def handleLook(self, tok):
+        loc = self.character.location
+        buffer = [loc.name]
+        buffer.append(loc.description)
+        if len(loc.characters) == 1:
+            buffer.append("You're alone")
+        elif len(loc.characters) == 2:
+            chars = loc.characters[:]
+            chars.remove(self.character)
+            buffer.append("%s is here."%chars[0].name)
+        else:
+            chars = loc.characters[:]
+            chars.remove(self.character)
+            buffer.append("%s are here."%(", ".join(chars)))
+        self.send("\n".join(buffer))
+        
+    def handleSpawn(self, tok):
+        self.handler = self.gmSpawner
+        self.handlerstate = 0
+        self.temp = {}
+        return self.handler(None)
+        
+    def handleOfftopic(self, tok):
+        if tok[-1][-1] == ')': 
+            tok[-1] = tok[-1][:-1]
+        message = "%s: %s"%(self.account.name," ".join(tok)[1:])
+        self.world.offtopic(message)
+        
+    def handleSay(self, tok):
+        pass
+        
+    def handleShout(self, tok):
+        pass
+        
+    def handleCreate(self, tok):
+        pass
+        
+    def handleGM(self, tok):
+        self.gamemaster = not self.gamemaster
+        
+    def handleCharlist(self, tok):
+        print "Listing chars"
+        chars = self.world.findOwner(self.account.name,self.world.characters)
+        if chars == None: 
+            return "You possess no characters.."
+        
+        buffer = ["You can possess the following characters"]
+        if not isinstance(chars,list): 
+            chars = [chars]
+        
+        for char in chars:
+            if char == self.character:
+                buffer.append("** %s - %s **"%(char.name,char.info))
+            else:
+                buffer.append("%s - %s"%(char.name,char.info))
+        self.offtopic('\n'.join(buffer))
+        
+    def handleIntroduce(self, tok):
+        name = " ".join(message[1:])
+        if len(name) < 2: return "Introduce as who?"
+        pass
+        
+    def handleMemorize(self, tok):
+        pass
+        
+    def handleStyle(self,tok):
+        self.account.style = not self.account.style
+        if self.account.style: return "You are now using MUD-style"
+        else: return "You are now using IRC-style"
+        self.db.save()
