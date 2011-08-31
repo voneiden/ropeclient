cf315d4fb5b5f97b562606f139d4ea51c3044c46
Matti Eiden <snaipperi@gmail.com>
2011-08-31 11:36:38 +0300

    TYPOFIX again

--- server/player.py
+++ server/player.py
@@ -38,7 +38,7 @@ class Player(object):
         self.password = None
         self.account = None
         self.typing = False
-        self.r = self.loginHandler
+        self.handler = self.loginHandler
         self.character = None
         self.gamemaster = False
         self.handlerstate = 1
