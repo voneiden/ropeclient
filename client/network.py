fd84c47991b6de443dee1c92db41206801d361c2
Matti Eiden <snaipperi@gmail.com>
2011-09-10 12:03:24 +0300

    Made CLR packet clear the offtopic window too \(FIX\)

--- client/network.py
+++ client/network.py
@@ -88,9 +88,9 @@ class Connection(LineReceiver):
                 self.window.textboxMain.delete(1.0, window.END)
                 self.window.textboxMain.config(state=window.DISABLED)
                 
-                self.window.offtopicMain.config(state=window.NORMAL)
-                self.window.offtopicMain.delete(1.0,window.END)
-                self.window.offtopicMain.config(state=window.DISABLED)
+                self.window.textboxOfftopic.config(state=window.NORMAL)
+                self.window.textboxOfftopic.delete(1.0,window.END)
+                self.window.textboxOfftopic.config(state=window.DISABLED)
             else:
                 print "Unknown tok",tok
         else: 
