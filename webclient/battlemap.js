4a455e679bab8c6f1dc7444b05f0b91c23481720
Matti Eiden <snaipperi@gmail.com>
2014-10-11 23:09:42 +0300

    Battlemap scrolling, rectangular shadows, lights, better integration to ropeclient

--- webclient/battlemap.js
+++ webclient/battlemap.js
@@ -4,9 +4,14 @@ var debug_start = new Date().getTime(); // Debug fps
 var debug_loops = 0
 var debug_delta_x = 0;
 var debug_delta_y = 0;
-function initialize() {
+var bm_settings_mask = true;
+var bm_view = [0, 0];  // Top left coordinates of the current view
+
+function bm_init() {
+    console.log("bm_init..");
     var canvas_scene = document.getElementById('battlemap-scene');
     var canvas_mask = document.getElementById('battlemap-mask');
+    var root = document.getElementById('root-battlemap');
 
     canvas_mask.width = canvas_scene.width;
     canvas_mask.height = canvas_scene.height;
@@ -17,15 +22,7 @@ function initialize() {
 
     load_resources();
 
-
-
-    $("#battlemap-mask").click(function(e){
-        var x = e.offsetX;
-        var y = e.offsetY;
-
-        console.log(x, y);
-    });
-
+    // TODO proper event handler for keyboard input
     $(document).keydown(function(e) {
         switch(e.which) {
             case 37: // left
@@ -44,12 +41,32 @@ function initialize() {
                 debug_delta_y += 1;
                 break;
 
-            default: return; // exit this handler for other keys
+            case 76:
+                bm_settings_mask = !bm_settings_mask;
+                console.log("MASK TOGGLE");
+                //draw_battlemap();
+                break;
+
+            default:
+                console.log(e.which);
+                return; // exit this handler for other keys
         }
-        console.warn(debug_delta_x, debug_delta_y)
+        //console.warn(debug_delta_x, debug_delta_y)
         draw_battlemap()
         e.preventDefault(); // prevent the default action (scroll / move caret)
     });
+
+    // Mouse events
+    canvas_mask.addEventListener('mousedown', function(event) { bm_mouse(1, event) }, false);
+    canvas_mask.addEventListener('mouseup', function(event) {bm_mouse(-1, event) }, false);
+    canvas_mask.addEventListener('mousemove', function(event) { bm_mouse(0, event) }, false);
+    window.addEventListener("resize", function(event) { bm_resize(event) });
+
+    // Disable right click context menu
+    canvas_mask.oncontextmenu = function() {
+        return false;
+    }
+
 
 }
 
@@ -59,13 +76,149 @@ function load_resources() {
     background_image = new Image();
     background_image.src = src;
     background_image.onload = function() {
+        bm_resize(null);
         draw_battlemap()
     };
-    //while (true ) {
-    //    draw_battlemap();
-    //}
 }
 
+/* Mouse tracking and commands */
+var bm_mouse_shadow_from = false;
+var bm_mouse_right_down = false;
+var bm_mouse_right_last = null;
+var bm_mouse_last = null;
+function bm_mouse(type, event) {
+    /*
+        type
+            1: down
+            0: move
+            -1: up
+     */
+
+    // Set this variable to true to redraw the map after event handler finishes
+    var request_draw = false;
+
+    // Store mouse movement
+    if (type == 0) {
+        bm_mouse_last = [event.offsetX, event.offsetY];
+
+        // If currently creating a shadow, update map
+        if (bm_mouse_shadow_from) {
+            request_draw = true;
+        }
+    }
+    // Handle left click
+    if (event.button == 0) {
+        // Down event and shift pressed
+        if (type == 1 && event.shiftKey) {
+            bm_mouse_shadow_from = [event.offsetX + bm_view[0], event.offsetY + bm_view[1]];
+            request_draw = true;
+        }
+        else if (type == -1 && bm_mouse_shadow_from) {
+            var x1 = bm_mouse_shadow_from[0];
+            var y1 = bm_mouse_shadow_from[1];
+            var x2 = event.offsetX + bm_view[0];
+            var y2 = event.offsetY + bm_view[1];
+            var dx = Math.abs(x2 - x1);
+            var dy = Math.abs(y2 - y1);
+
+            if (dx < 5 || dy < 5 || !event.shiftKey) {
+                console.log("Rectangle too small!");
+            }
+            else {
+                bm_block_rect.push([[x1, y1],
+                                    [x1, y2],
+                                    [x2, y2],
+                                    [x2, y1]]);
+            }
+
+            bm_mouse_shadow_from = false;
+            request_draw = true;
+        }
+    }
+    // Right click
+    else if (event.button == 2) {
+        // Right down
+        if (type == 1) {
+            bm_mouse_right_down = true;
+            bm_mouse_right_last = [event.offsetX, event.offsetY];
+        }
+        // Right up
+        else if (type == -1) {
+            bm_mouse_right_down = false;
+            bm_mouse_right_last = null;
+        }
+        // Right motion
+        else if (type == 0) {
+            var dx = bm_mouse_right_last[0] - event.offsetX;
+            var dy = bm_mouse_right_last[1] - event.offsetY;
+
+            bm_view[0] += dx;
+            bm_view[1] += dy;
+
+            // Limit view scrolling to background image dimensions
+            if (bm_view[0] < 0) { bm_view[0] = 0; }
+            if (bm_view[1] < 0) { bm_view[1] = 0; }
+
+            if (background_image.width && background_image.height) {
+                var canvas_scene = document.getElementById('battlemap-scene');
+                var max_x = background_image.width - canvas_scene.width;
+                var max_y = background_image.height - canvas_scene.height;
+
+                if (bm_view[0] > max_x) { bm_view[0] = max_x; }
+                if (bm_view[1] > max_y) { bm_view[1] = max_y; }
+            }
+
+            bm_mouse_right_last[0] = event.offsetX;
+            bm_mouse_right_last[1] = event.offsetY;
+
+            request_draw = true;
+        }
+    }
+
+    if (request_draw) {
+        draw_battlemap();
+    }
+    event.preventDefault()
+    return false;
+}
+
+function bm_resize(event) {
+    console.log("RES");
+    var canvas_scene = document.getElementById('battlemap-scene');
+    var canvas_mask = document.getElementById('battlemap-mask');
+    var root = $("#root-battlemap");
+    //var root = document.getElementById('root-battlemap');
+    //var w = root.clientWidth;
+    //var h = root.clientHeight;
+
+    var w = root.width();
+    var h = root.height();
+
+    if (w > background_image.width) { w = background_image.width }
+    if (h > background_image.height) { h = background_image.height }
+
+    // Resize canvas pixel area
+    canvas_scene.width = canvas_mask.width = w
+    canvas_scene.height = canvas_mask.height = h;
+
+    // Resize canvas CSS
+    canvas_scene = $("#battlemap-scene");
+    canvas_scene.width(w);
+    canvas_scene.height(h);
+
+    canvas_mask = $("#battlemap-mask");
+    canvas_mask.width(w);
+    canvas_mask.height(h);
+
+    // Redraw
+    draw_battlemap();
+}
+
+/* Main drawing function */
+var bm_block_rect = [[[229, 229],
+                      [264, 229],
+                      [264, 159],
+                      [229, 159]]]
 
 function draw_battlemap() {
     /*
@@ -91,14 +244,22 @@ function draw_battlemap() {
 
 
     // Draw background_image to scene
-    scene.drawImage(background_image, 0, 0);
+    scene.drawImage(background_image, -bm_view[0], -bm_view[1]);
 
 
     // Define mask ambient color
-    var ambient_color = [0, 0, 0];
-    var ambient_hex = color_to_hex(ambient_color, 1.0);
-    mask.fillStyle= ambient_hex;
-    mask.fillRect(0, 0, canvas_mask.width, canvas_mask.height);
+
+    if (bm_settings_mask) {
+        console.log("normal fill");
+        var ambient_color = [0, 0, 0];
+        var ambient_hex = color_to_hex(ambient_color, 1.0);
+        mask.fillStyle= ambient_hex;
+        mask.fillRect(0, 0, canvas_mask.width, canvas_mask.height);
+    }
+    else { //TODO: do this only once
+        console.log("Clear");
+        mask.clearRect(0, 0, canvas_mask.width, canvas_mask.height);
+    }
 
 
     // Light point sources
@@ -107,8 +268,8 @@ function draw_battlemap() {
     var lights = [[300, 300, 200, [0, 200, 0, 1]],
                   [400, 200, 200, [0, 0, 200, 1]],
                   [200, 200, 200, [200, 0, 0, 1]]];
-    var lights = [[300+debug_delta_x, 250+debug_delta_y, 250, [100, 100, 100, 1]],
-                  [200+debug_delta_x/2, 150+debug_delta_y/2, 50,  [100, 100, 100, 1]]];
+    var lights = [[300+debug_delta_x*5, 250+debug_delta_y*5, 250, [0, 150, 0, 1]],
+                  [200, 150, 100,  [150, 0, 0, 1]]];
     //var lights = [[300+debug_delta_x, 250+debug_delta_y, 250, [100, 100, 100, 1]]]
 
 
@@ -119,20 +280,20 @@ function draw_battlemap() {
         var light_outer = lights[i][2]
         var light_color = lights[i][3]
 
-        var light_dx = light_x - light_outer;
-        var light_dy = light_y - light_outer;
+        var light_dx = light_x - light_outer - bm_view[0];
+        var light_dy = light_y - light_outer - bm_view[1];
         var light_diameter = light_outer*2;
 
         canvas_light.width = light_diameter;
         canvas_light.height = light_diameter;
 
-        console.log(light.width,light.height);
-        console.log("DINGG");
+        //console.log(light.width,light.height);
+        //console.log("DINGG");
 
         // TODO: Do shadows and store them
         // Shadow blocks should be either rect or circles
         // Example rect shadow
-        shadow_polygons = generate_shadow_polygons(light_x, light_y, light_outer);
+        var shadow_polygons = generate_shadow_polygons(light_x, light_y, light_outer);
 
         // Clear light canvas
         light.globalCompositeOperation="source-over"
@@ -161,8 +322,8 @@ function draw_battlemap() {
                 var py = poly[i_point][1];
 
                 // Convert points relative to light
-                px = px - light_x + light_outer;
-                py = py - light_y + light_outer;
+                px = px - light_x + light_outer
+                py = py - light_y + light_outer
 
                 if (i_point == 0) { light.moveTo(px, py); }
                 else { light.lineTo(px, py); }
@@ -190,21 +351,37 @@ function draw_battlemap() {
 
 
     }
+
+    if (bm_mouse_shadow_from) {
+        mask.fillStyle = "rgba(255, 255, 255, 1)";
+        mask.strokeStyle = "rgba(255, 255, 255, 1)";
+        var x = bm_mouse_shadow_from[0] - bm_view[0];
+        var y = bm_mouse_shadow_from[1] - bm_view[1];
+        var w = bm_mouse_last[0] - x;
+        var h = bm_mouse_last[1] - y;
+        console.log(x, y, w, h);
+        mask.fillRect(x, y, w, h);
+        //mask.fillRect(bm_mouse_shadow_from[0]-2, bm_mouse_shadow_from[1]-2, 4, 4);
+
+    }
+
     var debug_end = new Date().getTime();
     $("#details").text(debug_loops / (debug_end - debug_start) * 1000 + "loops per second")
     debug_loops++;
+
+    if (debug_end - debug_start > 2000) {
+        debug_loops = 0;
+        debug_start = debug_end;
+    }
     //window.setTimeout("draw_battlemap()", 1);
 }
 
 function generate_shadow_polygons(x, y, max_distance) {
-    block_rect = [[[229, 229],
-                   [264, 229],
-                   [264, 159],
-                   [229, 159]]]
+
     polygons = []
     // Loop through every rectangle in block list
-    for (var i_rect=0; i_rect < block_rect.length; i_rect++) {
-        var rect = block_rect[i_rect];
+    for (var i_rect=0; i_rect < bm_block_rect.length; i_rect++) {
+        var rect = bm_block_rect[i_rect];
 
         // Check distance
         var in_range = false;
@@ -218,7 +395,7 @@ function generate_shadow_polygons(x, y, max_distance) {
             var distance = Math.sqrt(rx*rx + ry*ry);
             if (!in_range && distance < max_distance) {
                 in_range = true;
-                console.log("Block rect", i_rect, "is in range for light at",x,y,"with distance of", distance, "/", max_distance);
+                //console.log("Block rect", i_rect, "is in range for light at",x,y,"with distance of", distance, "/", max_distance);
             }
 
             var ux = rx / distance;
@@ -229,7 +406,7 @@ function generate_shadow_polygons(x, y, max_distance) {
         }
 
         if (!in_range) {
-            console.log("Block rect ", i_rect, " is not in range for light at ",x, " ", y);
+            //console.log("Block rect ", i_rect, " is not in range for light at ",x, " ", y);
             continue;
         }
 
@@ -243,7 +420,7 @@ function generate_shadow_polygons(x, y, max_distance) {
 
         // Next step is to sort the angles and do the step analysis
         angle_list.sort();
-        console.log("Ang",angle_list);
+        //console.log("Ang",angle_list);
         var step_dict = {};
         var step_list = [];
         var n_step;
@@ -258,7 +435,7 @@ function generate_shadow_polygons(x, y, max_distance) {
             i_angle = angle_list[i_step];
             n_angle = angle_list[n_step];
 
-            console.log(i_angle, n_angle);
+            //console.log(i_angle, n_angle);
 
             step = Math.abs(n_angle - i_angle);
             if (step >= Math.PI) {
@@ -272,14 +449,14 @@ function generate_shadow_polygons(x, y, max_distance) {
         // Sort the steps and grab the biggest step
         step_list.sort();
 
-        console.log(step_list);
-        console.log(step_dict[step_list[3]]);
+        //console.log(step_list);
+        //console.log(step_dict[step_list[3]]);
 
         var shadow_angles = step_dict[step_list[3]];
         var shadow_polygon = [];
 
-        console.log("Start",  angle_dict[shadow_angles[0]])
-        console.log("End",  angle_dict[shadow_angles[1]])
+        //console.log("Start",  angle_dict[shadow_angles[0]])
+        //console.log("End",  angle_dict[shadow_angles[1]])
         // Add first selected angle point
         var start = angle_dict[shadow_angles[0]];
         var start_x = start[0] + x; // Convert relative position to absolute position;
@@ -296,7 +473,7 @@ function generate_shadow_polygons(x, y, max_distance) {
         // Push projection point 1
         var proj1_x = start[2] * 1000 + x;
         var proj1_y = start[3] * 1000 + y;
-        console.log(proj1_x, proj1_y,start[2], start[3]);
+        //console.log(proj1_x, proj1_y,start[2], start[3]);
         shadow_polygon.push([proj1_x, proj1_y]);
 
          // Push projection point 2
@@ -321,4 +498,7 @@ function color_to_hex(color, alpha) {
     return "rgba(" + color[0] + ", " + color[1] + ", " + color[2] + ", " + alpha + ")";
 }
 
-$(document).ready(function(){ initialize() });
+function bm_absolute_coords_to_view(coords) {
+    return [coords[0] - bm_view[0], coords[1] - bm_view[1]];
+}
+$(document).ready(function(){ bm_init() });
