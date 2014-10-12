c29230b95986cc4fecef1bcb724a0c0f55adbc16
Matti Eiden <snaipperi@gmail.com>
2014-10-12 22:30:09 +0300

    Added intersection testing functions

--- webclient/battlemap.js
+++ webclient/battlemap.js
@@ -488,6 +488,76 @@ Battlemap.prototype.get_rgba = function(color, alpha) {
     return "rgba(" + color[0] + ", " + color[1] + ", " + color[2] + ", " + alpha + ")";
 }
 
+/* ========================================
+ * Battlemap.test_intersect_CCW(p1, p2, p3)
+ * ========================================
+ * Helper function for Battlemap.test_intersect_line_line()
+ * Source: http://stackoverflow.com/a/16725715/1744706
+ */
+Battlemap.prototype.test_intersect_CCW = function(p1, p2, p3) {
+  return (p3[1] - p1[1]) * (p2[0] - p1[0]) > (p2[1] - p1[1]) * (p3[0] - p1[0]);
+}
+
+/* ==================================================
+ * Battlemap.test_intersect_line_line(p1, p2, p3 ,p4)
+ * ==================================================
+ * Test if line segment [p1, p2] intersects with line
+ * segment [p3, p4].
+ */
+Battlemap.prototype.test_intersect_line_line = function(p1, p2, p3, p4) {
+  return (this.test_intersect_CCW(p1, p3, p4) != this.test_intersect_CCW(p2, p3, p4)) && (this.test_intersect_CCW(p1, p2, p3) != this.test_intersect_CCW(p1, p2, p4));
+}
+
+/* ===================================================
+ * Battlemap.test_intersect_point_poly(point, polygon)
+ * ===================================================
+ * Test if point is inside polygon
+ * Source: http://stackoverflow.com/a/2922778/1744706
+ */
+Battlemap.prototype.test_intersect_point_poly = function(point, polygon) {
+    var c = false;
+    for (var i= 0,j=polygon.length-1; i < polygon.length; j=i++) {
+        if (((polygon[i][1] > point[1]) != (polygon[j][1] > point[1])) &&
+            (point[0] < (polygon[j][0] - polygon[i][0]) *
+                        (point[1] - polygon[i][1]) / (polygon[j][1] - polygon[i][1]) + polygon[i][0])) {
+            c = !c;
+       }
+    }
+    return c;
+}
+
+/*
+ * Battlemap.test_intersect_point_circle(point, circle)
+ * ====================================================
+ * Test if point (x,y) is inside circle (x, y, radius)
+ */
+Battlemap.prototype.test_intersect_point_circle = function(point, circle) {
+    return Math.sqrt(Math.pow(point[0]-circle[0], 2) + Math.pow(point[1]-circle[1], 2 )) < circle[3];
+}
+
+/*
+ * Battlemap.test_intersect_line_circle(p1, p2, circle)
+ * ====================================================
+ * Test if line segment p1,p2 is intersects circle
+ * Source: http://stackoverflow.com/a/1084899/1744706
+ */
+Battlemap.prototype.test_intersect_line_circle = function(p1, p2, circle) {
+
+    var sx = p2[0] - p1[0];
+    var sy = p2[1] - p1[1];
+
+    var cx = p1[0] - circle[0];
+    var cy = p1[1] - circle[1];
+
+    var a = sx*sx + sy*sy;
+    var b = 2 * (cx * sx + cy * sy);
+    var c = cx*cx + cy*cy - circle[2] * circle[2];
+
+    var discriminant = b*b - 4*a*c;
+    if (discriminant < 0) { return false; }
+    else { return true; }
+}
+
 $(document).ready(function() {
     bm = new Battlemap();
     bm.init();
